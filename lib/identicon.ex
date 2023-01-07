7d290e5a3b6a510f19eb557fa9a80453f6064aac
RavenLycans <raven@lycans.dk>
2023-01-07 21:31:50 +0100

    Lesson 46 complete

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,13 +6,25 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(%Identicon.Image{hex: [red, green, blue | _rest]} = image) do
+  defp build_grid(%Identicon.Image{hex: hex} = image) do
+      grid = Enum.chunk_every(hex, 3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+
+      %Identicon.Image{image | grid: grid}
+  end
+
+  defp mirror_row([first, second | _rest] = row) do
+    row ++ [second, first]
+  end
+
+  defp pick_color(%Identicon.Image{hex: [red, green, blue | _rest]} = image) do
     %Identicon.Image{image | color: {red, green, blue}}
   end
 
-  def hash_input(input) do
+  defp hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
