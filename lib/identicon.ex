03c5a70378534251ec4af30fd7d6b77b52adb5c9
RavenLycans <raven@lycans.dk>
2023-01-07 20:58:54 +0100

    Lesson 42 complete

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,13 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [red, green, blue | _rest]} = image
+
+    [red, green, blue]
   end
 
   def hash_input(input) do
