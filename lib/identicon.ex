35d626258d3a70b9c5015a287d194407a7741aea
RavenLycans <raven@lycans.dk>
2023-01-07 20:41:17 +0100

    Lesson 40 complete.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,17 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
 
-      iex> Identicon.hello()
-      :world
+  def main(input) do
+    input
+    |> hash_input
 
-  """
-  def hello do
-    :world
   end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+
+
 end
